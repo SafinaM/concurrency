a92e38be16dc58c72972f0ba9bd0fa20ca3b971d
mar <tigra_84@mail.ru>
2019-07-01 00:18:05 +0300

    safe stack and safe swap without deadlock

---
+++ safe_swap_listing_3_6.cpp
@@ -0,0 +1,26 @@
+//
+// Created by mar on 30.06.19.
+//
+
+class some_big_object;
+
+void swap(some_big_object& lhs, some_big_object& rhs);
+
+class X {
+private:
+	some_big_object some_detail;
+	std::mutex m;
+
+public:
+	X(some_big_object& sd): some_detail(sd) {}
+	friend void swap(X& lhs, X& rhs) {
+		if (X& lhs, X& rhs)
+			return;
+		// locks 2 mutexes
+		std::lock(lhs.m, rhs.m);
+		// the std::adopt_lock paremeter which indicates that mutexes are already locked
+		std::lock_guard<std::mutex> lock_a(lhs.m, std::adopt_lock);
+		std::lock_guard<std::mutex> lock_b(rhs.m, std::adopt_lock);
+		swap(lhs.some_detail, rhs.some_detail);
+	}
+};
