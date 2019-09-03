e3ef9a007f0a467095df32d07fc96c4c3cf9a1d1
m.safina <tigra_84@mail.ru>
2019-09-03 15:46:23 +0300

    ping pong example on conditinal variables from oop point of view

--- ex_ping_pong_with_conf_variable.cpp
+++ ex_ping_pong_with_conf_variable.cpp
@@ -1,51 +1,112 @@
 //
 // Created by mar on 01.09.19.
 //
-
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 #include <thread>
 
-int main() {
-	bool notified = false;
-	bool done = false;
-	std::mutex m;
-	std::condition_variable cond;
-	std::chrono::milliseconds period(200);
-	auto ping = [&](const uint32_t num) {
-		while(!done) {
-			for (int i = 0; i < num; ++i) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(period));
-				std::cout << "Ping!" << " " << std::this_thread::get_id() << std::endl;
-				std::unique_lock<std::mutex>(m);
-				notified = true;
-				cond.notify_one();
+
+class PingPongPlayer {
+public:
+	PingPongPlayer(const std::chrono::milliseconds interval, const uint32_t number) :
+	_timeInterval(interval),
+	_numberOfRepetitions(number){}
+	
+	void pingPong() {
+		std::thread { [&](){ ping(); } }.detach();
+		std::thread { [&](){ pong(); } }.detach();
+	}
+	
+	~PingPongPlayer() {
+	
+	}
+
+private:
+	std::condition_variable _conditionVariable;
+	std::mutex _mutex;
+	std::chrono::milliseconds _timeInterval;
+	bool _finished = false;
+	bool _notified = false;
+	uint32_t _numberOfRepetitions;
+	
+	void ping() {
+		while(!_finished) {
+			for (int i = 0; i < _numberOfRepetitions; ++i) {
+				std::this_thread::sleep_for(std::chrono::milliseconds(_timeInterval));
+				std::cout << i << ") Ping!" << " " << std::this_thread::get_id() << std::endl;
+				std::unique_lock<std::mutex>(_mutex);
+				_notified = true;
+				_conditionVariable.notify_one();
 				std::this_thread::yield();
 			}
-			done = true;
-			cond.notify_one();
+			_finished = true;
+			_conditionVariable.notify_one();
 		}
 	};
 	
-	auto pong = [&]() {
-		while(!done) {
-			std::unique_lock<std::mutex> lock(m);
-			while (!notified) {
-				cond.wait(lock);
+	void pong(){
+		while(!_finished) {
+			std::unique_lock<std::mutex> lock(_mutex);
+			while (!_notified) {
+				_conditionVariable.wait(lock);
 			}
 			std::cout << "Pong!" << " " << std::this_thread::get_id() << std::endl;
-			notified = false;
-			std::this_thread::sleep_for(std::chrono::milliseconds(period));
+			_notified = false;
+			std::this_thread::sleep_for(std::chrono::milliseconds(_timeInterval));
+			std::this_thread::yield();
 		}
 	};
-	const uint32_t number = 5;
-	std::thread ping_t, pong_t;
-	ping_t = std::thread(ping, number);
-	pong_t = std::thread(pong);
-	ping_t.join();
-	pong_t.join();
+};
+
+int main() {
+	{
+		std::chrono::milliseconds ms(200);
+		PingPongPlayer player(ms, 20);
+		player.pingPong();
+		std::this_thread::sleep_for(std::chrono::milliseconds(10000));
+	}
+
+	
+	
+//	bool notified = false;
+//	bool done = false;
+//	std::mutex m;
+//	std::condition_variable cond;
+//	std::chrono::milliseconds period(200);
+//	auto ping = [&](const uint32_t num) {
+//		while(!done) {
+//			for (int i = 0; i < num; ++i) {
+//				std::this_thread::sleep_for(std::chrono::milliseconds(period));
+//				std::cout << "Ping!" << " " << std::this_thread::get_id() << std::endl;
+//				std::unique_lock<std::mutex>(m);
+//				notified = true;
+//				cond.notify_one();
+//				std::this_thread::yield();
+//			}
+//			done = true;
+//			cond.notify_one();
+//		}
+//	};
+//
+//	auto pong = [&]() {
+//		while(!done) {
+//			std::unique_lock<std::mutex> lock(m);
+//			while (!notified) {
+//				cond.wait(lock);
+//			}
+//			std::cout << "Pong!" << " " << std::this_thread::get_id() << std::endl;
+//			notified = false;
+//			std::this_thread::sleep_for(std::chrono::milliseconds(period));
+//		}
+//	};
+//	const uint32_t number = 5;
+//	std::thread ping_t, pong_t;
+//	ping_t = std::thread(ping, number);
+//	pong_t = std::thread(pong);
+//	ping_t.join();
+//	pong_t.join();
 	
 	return 0;
 }
