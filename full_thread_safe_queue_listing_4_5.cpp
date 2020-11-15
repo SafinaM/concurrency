ec842d2768c551b580842d76b7b1a78b56248aaf
mar <tigra_84@mail.ru>
2020-11-15 18:02:19 +0300

    listing 4.5 thread safe queue full class with example

--- full_thread_safe_queue_listing_4_5.cpp
+++ full_thread_safe_queue_listing_4_5.cpp
@@ -1,67 +1,75 @@
-#include <condition_variable>
+//
+// Created by mar on 12.11.2020.
+//
+
+#include "common_listing_4.h"
 #include <thread>
 #include <queue>
+#include <mutex>
+#include <condition_variable>
 #include <iostream>
-#include "common_listing_4.h"
 
-template<typename T>
-class threadsafe_queue {
+template <typename T>
+class thread_safe_queue {
 public:
-	threadsafe_queue() = default;
-	threadsafe_queue(const threadsafe_queue&) = delete;
-	threadsafe_queue& operator=(const threadsafe_queue&) = delete;
-	void push(T new_value) {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		m_queue.push(new_value);
+	thread_safe_queue() = default;
+	thread_safe_queue(const thread_safe_queue& other) {
+		std::lock_guard<std::mutex> lk(other.mut);
+		m_queue = other.data_queue;
+	}
+
+	void push(const T& value) {
+		std::lock_guard<std::mutex>lockGuard(m_mutex);
+		m_queue.push(value);
 		m_cond.notify_one();
 	}
+
 	bool try_pop(T& value) {
-		std::lock_guard<std::mutex> lock(m_mutex);
 		if (m_queue.empty())
 			return false;
 		value = m_queue.front();
-		return true;
+		m_queue.pop();
 	}
+
 	std::shared_ptr<T> try_pop() {
-		std::lock_guard<std::mutex> lock(m_mutex);
 		if (m_queue.empty())
-			return std::shared_ptr<Data>();
-		std::shared_ptr<Data> data_ptr = std::make_shared<Data>(m_queue.front());
-		return data_ptr;
+			return false;
+		std::shared_ptr<T> ptr = std::make_shared(m_queue.front());
+		m_queue.pop();
+		return ptr;
 	}
+
 	void wait_and_pop(T& value) {
 		std::unique_lock<std::mutex> uniqueLock(m_mutex);
-		m_cond.wait(uniqueLock, [this] {
-			return !m_queue.empty(); });
+		m_cond.wait(uniqueLock, [this]() {
+			return !m_queue.empty();
+		});
 		value = m_queue.front();
 		m_queue.pop();
 	}
-	std::shared_ptr<T> wait_and_pop(){
+
+	std::shared_ptr<T> wait_and_pop(const T& value) {
 		std::unique_lock<std::mutex> uniqueLock(m_mutex);
-		m_cond.wait(uniqueLock, [this] {
-			return !m_queue.empty(); });
-		std::shared_ptr<Data> data_ptr = std::make_shared(m_queue.front());
+		m_cond.wait(uniqueLock, [this]() {
+			return !m_queue.empty();
+		});
+		std::shared_ptr<T> ptr = std::make_shared(m_queue.front());
 		m_queue.pop();
-		return data_ptr;
+		return ptr;
 	}
 
-	bool empty() const {
-		std::lock_guard<std::mutex> lock(m_mutex);
+	bool empty() {
+		std::lock_guard<std::mutex> lockGuard(m_mutex);
 		return m_queue.empty();
 	}
 
-	int size() const {
-		std::lock_guard<std::mutex> lock(m_mutex);
-		return m_queue.size();
-	}
-
 private:
+	mutable std::mutex m_mutex;
 	std::condition_variable m_cond;
-	std::mutex m_mutex;
 	std::queue<T> m_queue;
 };
 
-threadsafe_queue<Data> data_queue;
+thread_safe_queue<Data> data_queue;
 
 void data_preparation_thread() {
 	while(more_data_prepare()) {
